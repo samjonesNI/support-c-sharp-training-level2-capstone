568b956f4873a674d922e272711be09a2e2d1d5f
samjonesNI <59934367+samjonesNI@users.noreply.github.com>
2020-07-31 10:45:09 -0500

    Major refactor

--- Source/L2CapstoneProject/Beamformer.cs
+++ Source/L2CapstoneProject/Beamformer.cs
@@ -12,13 +12,15 @@ namespace L2CapstoneProject
 {
     public abstract class Beamformer
     {
+        //Overall flow method that runs the beamformer
+        public abstract void Run();
+        
         //DUT control methods
         public abstract bool ConnectDUT();
         public abstract void DisconnectDUT();
 
         //Used to change current pao for stepped beamformer
         public virtual void WriteOffset(PhaseAmplitudeOffset pao, double power) { }
-        public abstract void StimulateDUT();
         //Loads a list of pao's into memory for the sequenced beamformer
         public virtual void WriteSequence(List<PhaseAmplitudeOffset> paoList){ }
         public virtual void InitiateSequence() { }
@@ -29,17 +31,34 @@ namespace L2CapstoneProject
     }
     public class SimulatedSteppedBeamformer : Beamformer
     {
-        public SimulatedSteppedBeamformer(NIRfsg rfsg)
+        public SimulatedSteppedBeamformer(InstrumentConfig instrConfig)
         {
-            Rfsg = rfsg;
+            InstrConfig = instrConfig;
             Rfsg.Triggers.StartTrigger.ExportedOutputTerminal = RfsgStartTriggerExportedOutputTerminal.PxiTriggerLine0;
         }
 
         public NIRfsg Rfsg { get; set; }
+        public InstrumentConfig InstrConfig { get; set; }
+
+        public override void Run()
+        {
+            ConnectDUT();
+            Rfsg?.Initiate();
+            Rfsg.CheckGenerationStatus();
+            GenerateOffsets();
+            DisconnectDUT();
+
+
+        }
+
+        
 
         public override bool ConnectDUT()
         {
-            //Open session, set up configuration
+            Rfsg = new NIRfsg(InstrConfig.rfsgResourceName, true, false);
+            Rfsg.RF.Configure(InstrConfig.frequency, InstrConfig.power);
+            // TODO: Add this code back to the main form so that warnings are properly wired through
+            //Rfsg.DriverOperation.Warning += new EventHandler<RfsgWarningEventArgs>(DriverOperation_Warning);
             return true;
         }
 
@@ -52,22 +71,23 @@ namespace L2CapstoneProject
             //Close session
         }
 
-        public override void StimulateDUT()
-        {
-            //Begin generation of CW
-            
-            Rfsg?.Initiate();
-            Rfsg.CheckGenerationStatus();
-
-        }
-
-        public override void WriteOffset(PhaseAmplitudeOffset pao, double power)
+        public void WriteOffset(PhaseAmplitudeOffset pao)
         {
             Rfsg.Abort();
             Rfsg.Initiate();
-            Rfsg.RF.PowerLevel = power + pao.Amplitude;
+            Rfsg.RF.PowerLevel = InstrConfig.power + pao.Amplitude;
             Rfsg.RF.PhaseOffset = pao.Phase;
-        }      
+        }
+        
+        public void GenerateOffsets()
+        {
+            foreach (PhaseAmplitudeOffset pao in InstrConfig.PAOList)
+            {
+                WriteOffset(pao);
+                //measure();
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
         
     }
 }
